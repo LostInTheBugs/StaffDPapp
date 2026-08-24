fe5b3b22b7f83dadefcd487b117d685395e3411a
LostInTheBugs <LostInTheBugs@users.noreply.github.com>
2026-08-24 22:11:43 +0200

    release: bump displayed version in Footer and VersionCheck to 2026.08.031-pre

--- frontend/src/components/VersionCheck.tsx
+++ frontend/src/components/VersionCheck.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 
 /** Version locale (synchronisée avec le footer / les releases). */
-const LOCAL_VERSION = '2026.08.030'
+const LOCAL_VERSION = '2026.08.031-pre'
 
 function cmpVersions(a: string, b: string): number {
   const pa = a.replace(/^v/, '').split(/[.\-]/).map(n => parseInt(n) || 0)
